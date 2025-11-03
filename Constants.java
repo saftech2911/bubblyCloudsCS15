3c97b0e9a17f5120f444a0fda852180c274a1c0a
Safwan Intisar <73576169+saftech2911@users.noreply.github.com>
2025-11-02 19:38:26 -0500

    Add Constants class for scene and bubble settings

---
+++ Constants.java
@@ -0,0 +1,19 @@
+package codeAlong6;
+
+import javafx.scene.paint.Color;
+
+public class Constants {
+    public static final int SCENE_WIDTH = 900;
+    public static final int SCENE_HEIGHT = 600;
+
+    public static final int BUBBLE_SIZE = 15;
+    public static final Color BUBBLE_COLOR = Color.WHITE;
+
+    public static final int NUMBER_BUBBLE = 4;
+
+    public static final int[][] CLOUD_1 = {{205,300},{225,300},{245,300},{215,280}};
+
+    public static final int[][] CLOUD_2 = {{255,350},{275,350},{295,350},{265,325}};
+
+    public static final int BUBBLE_OFFSET = 15;
+}
